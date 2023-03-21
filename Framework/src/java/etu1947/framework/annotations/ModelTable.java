003fa6c1d26065b7bb7e8430edb2a1d57bfe0321
Sanjaratianaa <119104188+Sanjaratianaa@users.noreply.github.com>
2023-03-21 04:04:37 -0600

    Add files via upload

---
+++ Framework/src/java/etu1947/framework/annotations/ModelTable.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package etu1947.framework.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author layah
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ModelTable {
+    
+}
