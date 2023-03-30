f28dad8663f7829726391073825c51e4695f23dd
Sanjaratianaa <119104188+Sanjaratianaa@users.noreply.github.com>
2023-03-30 15:35:52 +0300

    sh tokony hapetraka any @ home/layah

--- Framework/src/java/etu1947/framework/utile/FonctionsUtile.java
+++ Framework/src/java/etu1947/framework/utile/FonctionsUtile.java
@@ -8,19 +8,24 @@ import etu1947.framework.annotations.Url;
 import etu1947.framework.mapping.Mapping;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author layah
  */
 public class FonctionsUtile {
+    
     public static ArrayList<Class<?>> getClasses(String packageName, HashMap<String, Mapping> urlMapping) throws ClassNotFoundException, IOException, URISyntaxException {
+        
         ArrayList<Class<?>> classes = new ArrayList<>();
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         String path = packageName.replace('.', '/');
@@ -29,16 +34,16 @@ public class FonctionsUtile {
         while (resources.hasMoreElements()) {
             URL resource = resources.nextElement();
             File file = new File(resource.toURI());
-//            if (file.isDirectory()) {
+            if (file.isDirectory()) {
                 for (File child : file.listFiles()) {
                     if(child.isFile()){
                         String className = packageName + "." + child.getName().split("\\.")[0];
                         classes.add(Class.forName(className));
-                        System.out.println(className);
+                        System.out.println("coUCOUUUUU : "+className);
                         getMethods(Class.forName(className), urlMapping);
                     }
                     classes.addAll(getClasses(packageName + "." + child.getName().split("\\.")[0], urlMapping));
-//                }
+                }
             } 
         }
         return classes;
@@ -50,7 +55,6 @@ public class FonctionsUtile {
      * scan the classes and in the meantime scan the methods in it 
      * @param klass is the class that will have the methods to retrieve
      * @param urlMapping is the HashMap object that will be filled as requested by Mr Naina
-     * @param out just a printer object in order to test
      */
     public static void getMethods(Class<?> klass, HashMap<String, Mapping> urlMapping){
         for(Method method : klass.getDeclaredMethods()){
@@ -58,8 +62,20 @@ public class FonctionsUtile {
                 Url annotation = method.getAnnotation(Url.class);
                 String url  = annotation.value();
                 Mapping map = new Mapping(klass.getName(), method.getName());
+                System.out.println("methodeeee: "+method.getName());
                 urlMapping.put(url, map);
             }
         }        
     }
+    
+    /**
+     * also this function will be called above in order to : 
+     * cast to an object
+     * @param klass is the class that will have the methods to retrieve
+     * @param urlMapping is the HashMap object that will be filled as requested by Mr Naina
+     */
+//    public static Object castClasseToObjetc(HashMap<String, Mapping> urlMapping){
+//        Class classe = 
+//        return objet;
+//    }
 }
---
+++ TestFramework/web/test.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : test
+    Created on : Mar 28, 2023, 1:44:07 AM
+    Author     : layah
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <p>Tongasoa ato aminay!</p>
+    </body>
+</html>
